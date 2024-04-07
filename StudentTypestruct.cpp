1ee645456cb7d22c722cd0980139d2e68b7b9902
leandawnleandawn <lyndonallensales@gmail.com>
2024-04-07 21:20:05 +0800

    Added exercises on struct

--- StudentTypestruct.cpp
+++ StudentTypestruct.cpp
@@ -11,12 +11,23 @@ struct studentType{
 void readStudentData(studentType &student_structure);
 int main(){
 
-	studentType student_Names[1];
+	studentType student_Names[3];
 
-	student_Names[0].studentFN = "KIm";
-	student_Names[0].studentLN = "bob";
+	std::string firstNames[] = {"KIm", "Angel", "Steven"};
+	std::string lastNames[] = {"das", "fsa", "goa"};
+	int grades[] = {1,2,4};
+	char grade[] = {'A','B','C'};
 
-	readStudentData(student_Names[0]);
+	for (int i = 0; i < 3; i++){
+		student_Names[i].studentFN =firstNames[i];
+		student_Names[i].studentLN = lastNames[i];
+		student_Names[i].testScore = grades[i];
+		student_Names[i].grade = grade[i];
+	}
+	 
+	for (int i = 0; i < 3; i++){
+		readStudentData(student_Names[i]);
+	}
 
 
 
@@ -27,9 +38,9 @@ int main(){
 
 void readStudentData(studentType &student_structure){
 
-	std::cout << "First Name" << student_structure.studentFN << std::endl;
-	std::cout << "Last Name" << student_structure.studentLN << std::endl;
-	std::cout << "Test Score" << student_structure.testScore << std::endl;
-	std::cout << "Grade" << student_structure.grade << std::endl;
+	std::cout << "First Name " << student_structure.studentFN << std::endl;
+	std::cout << "Last Name " << student_structure.studentLN << std::endl;
+	std::cout << "Test Score " << student_structure.testScore << std::endl;
+	std::cout << "Grade " << student_structure.grade << std::endl;
 
 }
